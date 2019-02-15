b6905cecb5e7effc9ea9ef64c168eb3d26423fe4
fgrabscheit <Felix.Grabscheit@gmail.com>
2019-02-15 18:31:17 +0100

    First tests of sockjs

--- src/machine-status.js
+++ src/machine-status.js
@@ -17,15 +17,23 @@ module.exports = {
     setMachineStatus: function () {
         setMachineStatus();
     },
-    setMachineWatch: function () {
-        gpio18.watch((err, value) => {
-            machineStatus = value;
-            if(machineStatus){ //<- machine was turned on
-                setTimestamp();
-            } else { //<- machine was turned off
-                timestamp = null;
-            }
+    setMachineWatch: function (sockjs_echo) {
+        let timeoutID;
+        sockjs_echo.on('connection', conn => {
+            gpio18.watch((err, value) => {
+                machineStatus = value;
+                if(machineStatus){ //<- machine was turned on
+                    setTimestamp();
+                    conn.write('Kaffeemaschine an!');
+                    clearInterval(timeoutID);
+                } else { //<- machine was turned off
+                    timeoutID = setTimeout(()=> {
+                        timestamp = null;
+                    }, 1000 * 60 * 5);
+                }
+            });
         });
+
     }
 };
 
--- src/server.js
+++ src/server.js
@@ -14,9 +14,6 @@ const sockjs_opts = {
 };
 
 const sockjs_echo = sockjs.createServer(sockjs_opts);
-sockjs_echo.on('connection', conn => {
-    conn.on('data', msg => conn.write(msg + 'test'));
-});
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
