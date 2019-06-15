56c64508480ea90f84170bda47f789aa35bac1ec
Felix Grab <felix.grabscheit@gmail.com>
2019-06-15 23:59:58 +0200

    Added new rest controller to redirect for iPhone

--- src/restController.js
+++ src/restController.js
@@ -18,9 +18,9 @@ module.exports = {
                 res.status(401)
                     .send(`Sorry, but you need to provide a client certificate to continue.`)
             }
-        })
+        });
 
-        app.get('/authenticate/iPhone', (req, res) => {
+        app.get('/authenticateiPhone', (req, res) => {
             const cert = req.connection.getPeerCertificate();
             if (req.client.authorized) {
                 res.redirect(301, 'http://adminconsole.felix-grabscheit.de')
@@ -31,103 +31,105 @@ module.exports = {
                 res.status(401)
                     .send(`Sorry, but you need to provide a client certificate to continue.`)
             }
-            /**
-             * Service to switch the status of the machine
-             * returns a timestamp when the machine was started
-             */
-            app.put('/machineStatus', function (req, res) {
-                if (req.client.authorized) {
-                    machineStatus.setMachineStatus();
-                    res.send();
-                } else if (cert.subject) {
-                    res.status(403)
-                        .send(`Not permitted`)
-                } else {
-                    res.status(401)
-                        .send(`No client-certificate passed`)
-                }
-            });
+        });
 
-            /**
-             * Service to switch the status of the machine
-             * returns a timestamp when the machine was started
-             */
-            app.get('/machineStatus/siri', function (req, res) {
-                if (req.client.authorized) {
-                    machineStatus.setMachineStatus();
-                    res.send();
-                } else if (cert.subject) {
-                    res.status(403)
-                        .send(`Not permitted`)
-                } else {
-                    res.status(401)
-                        .send(`No client-certificate passed`)
-                }
-            });
-            app.put('/startBrewing', function (req, res) {
-                if (req.client.authorized) {
-                    brewCoffee.startBrewing(req.body.brewTime);
-                    res.send();
-                } else if (cert.subject) {
-                    res.status(403)
-                        .send(`Not permitted`)
-                } else {
-                    res.status(401)
-                        .send(`No client-certificate passed`)
-                }
-            });
+        /**
+         * Service to switch the status of the machine
+         * returns a timestamp when the machine was started
+         */
+        app.put('/machineStatus', function (req, res) {
+            if (req.client.authorized) {
+                machineStatus.setMachineStatus();
+                res.send();
+            } else if (cert.subject) {
+                res.status(403)
+                    .send(`Not permitted`)
+            } else {
+                res.status(401)
+                    .send(`No client-certificate passed`)
+            }
+        });
 
-            app.put('/cancelBrewing', function (req, res) {
-                if (req.client.authorized) {
-                    brewCoffee.cancelBrewing();
-                    res.send();
-                } else if (cert.subject) {
-                    res.status(403)
-                        .send(`Not permitted`)
-                } else {
-                    res.status(401)
-                        .send(`No client-certificate passed`)
-                }
+        /**
+         * Service to switch the status of the machine
+         * returns a timestamp when the machine was started
+         */
+        app.get('/machineStatus/siri', function (req, res) {
+            if (req.client.authorized) {
+                machineStatus.setMachineStatus();
+                res.send();
+            } else if (cert.subject) {
+                res.status(403)
+                    .send(`Not permitted`)
+            } else {
+                res.status(401)
+                    .send(`No client-certificate passed`)
+            }
+        });
+        app.put('/startBrewing', function (req, res) {
+            if (req.client.authorized) {
+                brewCoffee.startBrewing(req.body.brewTime);
+                res.send();
+            } else if (cert.subject) {
+                res.status(403)
+                    .send(`Not permitted`)
+            } else {
+                res.status(401)
+                    .send(`No client-certificate passed`)
+            }
+        });
 
-            });
+        app.put('/cancelBrewing', function (req, res) {
+            if (req.client.authorized) {
+                brewCoffee.cancelBrewing();
+                res.send();
+            } else if (cert.subject) {
+                res.status(403)
+                    .send(`Not permitted`)
+            } else {
+                res.status(401)
+                    .send(`No client-certificate passed`)
+            }
 
-            app.put('/backFlush', function (req, res) {
-                if (req.client.authorized) {
-                    machineMaintenance.startBackflush();
-                    res.send();
-                } else if (cert.subject) {
-                    res.status(403)
-                        .send(`Not permitted`)
-                } else {
-                    res.status(401)
-                        .send(`No client-certificate passed`)
-                }
-            });
+        });
 
-            app.put('/cancelMaintenance', function (req, res) {
-                if (req.client.authorized) {
-                    machineMaintenance.cancelMaintenance();
-                    res.send();
-                } else if (cert.subject) {
-                    res.status(403)
-                        .send(`Not permitted`)
-                } else {
-                    res.status(401)
-                        .send(`No client-certificate passed`)
-                }
-            });
+        app.put('/backFlush', function (req, res) {
+            if (req.client.authorized) {
+                machineMaintenance.startBackflush();
+                res.send();
+            } else if (cert.subject) {
+                res.status(403)
+                    .send(`Not permitted`)
+            } else {
+                res.status(401)
+                    .send(`No client-certificate passed`)
+            }
+        });
 
-            app.put('/antiLiming', function (req, res) {
-                if (req.client.authorized) {
-                    machineMaintenance.startAntiLiming();
-                    res.send();
-                } else if (cert.subject) {
-                    res.status(403)
-                        .send(`Not permitted`)
-                } else {
-                    res.status(401)
-                        .send(`No client-certificate passed`)
-                }
-            });
-        }
-    };
+        app.put('/cancelMaintenance', function (req, res) {
+            if (req.client.authorized) {
+                machineMaintenance.cancelMaintenance();
+                res.send();
+            } else if (cert.subject) {
+                res.status(403)
+                    .send(`Not permitted`)
+            } else {
+                res.status(401)
+                    .send(`No client-certificate passed`)
+            }
+        });
+
+        app.put('/antiLiming', function (req, res) {
+            if (req.client.authorized) {
+                machineMaintenance.startAntiLiming();
+                res.send();
+            } else if (cert.subject) {
+                res.status(403)
+                    .send(`Not permitted`)
+            } else {
+                res.status(401)
+                    .send(`No client-certificate passed`)
+            }
+        });
+    }
+};
